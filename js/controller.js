3f4bac225519a9e83f905e3e226a79634505fb99
Jason Wu <me@jasonalvinwu.com>
2015-08-18 00:03:14 -0700

    Changed repeating assignments

--- js/controller.js
+++ js/controller.js
@@ -26,11 +26,11 @@ ctrl.controller('JIRA', function($scope) {
 
     $scope.assignTickets = function() {
         for (var ticket in $scope.ticketAssignmentArray) {
-            console.log("This is the ticket: " + ticket);
             var assignedUser = $scope.userAssignmentArray[Math.floor(Math.random()*$scope.userAssignmentArray.length)];
             $scope.assignments.push({"ticket" : $scope.ticketAssignmentArray[ticket], "member" : assignedUser});
             $scope.userAssignmentArray.splice($scope.userAssignmentArray.indexOf(assignedUser), 1);
         }
+        $scope.ticketAssignmentArray = [];
     }
 
 });
